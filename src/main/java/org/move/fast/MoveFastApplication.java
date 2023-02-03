51445e480b26c88a493b0d4c2dbd76eac7dc93c5
mecree <yin123456@>
2023-02-03 22:08:43 +0800

    完成大体逻辑

--- src/main/java/org/move/fast/MoveFastApplication.java
+++ src/main/java/org/move/fast/MoveFastApplication.java
@@ -3,12 +3,14 @@ package org.move.fast;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author sgw
  */
 @SpringBootApplication
 @MapperScan("org.move.fast.module.mapper")
+@EnableScheduling
 public class MoveFastApplication {
 
     public static void main(String[] args) {
