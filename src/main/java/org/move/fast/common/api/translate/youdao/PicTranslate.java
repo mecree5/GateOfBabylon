4cc3072074afadc4b3c983e0fb304fa3615c38d3
yinshijie <mecree@foxmail.com>
2023-02-23 17:27:32 +0800

    修改日志工具类

--- src/main/java/org/move/fast/common/api/translate/youdao/PicTranslate.java
+++ src/main/java/org/move/fast/common/api/translate/youdao/PicTranslate.java
@@ -3,15 +3,16 @@ package org.move.fast.common.api.translate.youdao;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.http.HttpRequest;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.move.fast.config.ReadConf;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class PicTranslate {
 
-    //文本翻译URL
     private static final String URL = "https://openapi.youdao.com/ocrtransapi";
     //应用ID
     private static final String APP_KEY = ReadConf.getConfValue("gateOfBabylon.api.youdao.ocr-trans.key");
@@ -19,7 +20,7 @@ public class PicTranslate {
     private static final String APP_SECRET = ReadConf.getConfValue("gateOfBabylon.api.youdao.ocr-trans.secret");
 
 
-    public static String trans(String base64) {
+    public static Map<String, String> trans(String base64) {
 
         Map<String, Object> params = new HashMap<>();
 
@@ -34,10 +35,19 @@ public class PicTranslate {
         params.put("q", base64);
 
         JSONObject rspJson = JSON.parseObject(HttpRequest.post(URL).form(params).execute().body());
+
         if (!Common.suc_code.equals(rspJson.getString("errorCode"))) {
             return null;
         }
-        return rspJson.getJSONArray("resRegions").getJSONObject(0).getString("tranContent");
+
+        LinkedHashMap<String, String> answer = new LinkedHashMap<>();
+
+        JSONArray resRegions = rspJson.getJSONArray("resRegions");
+        for (int i = 0; i < resRegions.size(); i++) {
+            answer.put("第" + i + 1 + "行:", resRegions.getJSONObject(i).getString("tranContent"));
+        }
+
+        return answer;
     }
 
 }
--- src/main/java/org/move/fast/common/utils/Log.java
+++ src/main/java/org/move/fast/common/utils/Log.java
@@ -5,12 +5,23 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.file.FileWriter;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 public class Log {
 
-    public static String LOG_PATH = System.getProperty("user.dir") + "\\log\\";
+    private static final String LOG_PATH = System.getProperty("user.dir") + "\\log\\";
 
-    public static String LAST_WRITE_TIME;
+    private static String LAST_WRITE_TIME;
+
+    private static final List<String> exceptionMsgMaskList = Collections.singletonList(
+            "Connection reset by peer" //客户端中断请求,tomcat报错,不影响业务
+    );
+
+    private enum Grade{
+        INFO,
+        ERROR,
+    }
 
     static {
         String[] list = new File(LOG_PATH).list();
@@ -21,6 +32,10 @@ public class Log {
     }
 
     public static void printAndWrite(String targetStr) {
+        printAndWrite(Grade.INFO, targetStr);
+    }
+
+    public static void printAndWrite(Grade grade,String targetStr) {
         String now = DateUtil.now();
         String nowDate = now.replaceAll("-", "").substring(0, 8);
         String path = LOG_PATH + nowDate + ".txt";
@@ -29,12 +44,17 @@ public class Log {
             LAST_WRITE_TIME = nowDate;
         }
         FileWriter writer = new FileWriter(path);
-        targetStr = "[" + now + "]" + targetStr;
-        System.err.println(targetStr);
+        targetStr = "[" + now + "]" + "[" + grade.name() + "]" + targetStr;
+        System.out.println(targetStr);
         writer.append(targetStr + "\r\n");
     }
 
     public static void printAndWrite(Exception exception) {
+
+        if (exceptionMsgMaskList.contains(exception.getMessage())) {
+            return;
+        }
+
         StringBuilder log = new StringBuilder(exception.getClass().toString());
         log.append(":").append(exception.getMessage());
         log.append("\r\n");
@@ -44,7 +64,7 @@ public class Log {
             log.append("    at  ").append(traceElement).append("\r\n");
         }
 
-        Log.printAndWrite(log.toString());
+        Log.printAndWrite(Grade.ERROR, log.toString());
     }
 
 }
