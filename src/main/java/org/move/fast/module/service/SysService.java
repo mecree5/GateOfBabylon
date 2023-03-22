cd2f5c84d11fdf0c407e07b9875acfda52f2bc21
yinshijie <mecree@foxmail.com>
2023-03-22 11:13:54 +0800

    修改api页面排版

--- src/main/java/org/move/fast/module/service/SysService.java
+++ src/main/java/org/move/fast/module/service/SysService.java
@@ -1,12 +1,16 @@
 package org.move.fast.module.service;
 
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import org.move.fast.common.api.vpn.VpnTypeEnum;
 import org.move.fast.common.utils.string.Html;
 import org.move.fast.module.entity.auto.SysConf;
+import org.move.fast.module.entity.auto.VpnCrawler;
 import org.move.fast.module.mapper.auto.SysConfMapper;
+import org.move.fast.module.mapper.auto.VpnCrawlerMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -18,38 +22,55 @@ public class SysService {
     @Resource
     SysConfMapper sysConfMapper;
 
-    public String getApiList(String url) {
+    @Resource
+    VpnCrawlerMapper vpnCrawlerMapper;
 
-        JSONObject getVmess = new JSONObject(true);
-        getVmess.put("DABAI", url + "/vpn/down/v2ray");
-        getVmess.put("GITHUB1", url + "/vpn/cd/v2ray/1");
-        getVmess.put("GITHUB2", url + "/vpn/cd/v2ray/2");
+    public String getApiList(String url) {
 
         JSONObject getRssUrl = new JSONObject(true);
         getRssUrl.put("RssUrl", url + "/vpn/stockUp/#{num}");
 
+        JSONObject getVmess = getVmessList(url);
+        JSONArray sysConfList = getSysConfList(url);
+
         JSONObject funcList = new JSONObject(true);
         funcList.put("添加订阅地址", getVmess);
         funcList.put("获取订阅地址", getRssUrl);
+        funcList.put("修改设置", sysConfList.get(1));
 
         JSONObject result = new JSONObject(true);
         result.put("功能列表", funcList);
-        result.put("系统设置", getSysConfList(url));
 
-        return Html.getHeadHtml() + Html.objToHtml(result);
+        return Html.getHeadHtml() + Html.objToHtml(sysConfList.get(0)) + Html.objToHtml(result);
     }
 
-    private JSONObject getSysConfList(String url) {
+    private JSONObject getVmessList(String url) {
+        JSONObject vmessList = new JSONObject(true);
+        for (VpnTypeEnum value : VpnTypeEnum.values()) {
+            vmessList.put("DABAI-" + value.getName(), url + "/vpn/down/" + value.getName());
+        }
+        for (VpnCrawler vpnCrawler : vpnCrawlerMapper.selectList(new QueryWrapper<>())) {
+            vmessList.put("GITHUB-" + vpnCrawler.getId(), url + "/vpn/cd/v2ray/" + vpnCrawler.getId());
+        }
+        return vmessList;
+    }
+
+    private JSONArray getSysConfList(String url) {
         JSONObject nowSysConfList = new JSONObject(true);
-        JSONObject upSysConfList = new JSONObject();
+        JSONObject upSysConfList = new JSONObject(true);
+        JSONObject nowSysConf = new JSONObject(true);
         for (SysConf sysConf : sysConfMapper.selectList(new QueryWrapper<>())) {
             String confKey = sysConf.getConfKey();
-            nowSysConfList.put(sysConf.getConfRemark(), confKey + "-->" + sysConf.getConfVal());
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put(confKey, sysConf.getConfVal());
+            nowSysConf.put(sysConf.getConfRemark(), jsonObject);
             upSysConfList.put(sysConf.getConfRemark(), url + "/sys/upset/" + confKey + "/#{num}");
         }
-        JSONObject sysConfList = new JSONObject(true);
-        sysConfList.put("当前设置", nowSysConfList);
-        sysConfList.put("修改设置", upSysConfList);
+        nowSysConfList.put("当前设置", nowSysConf);
+
+        JSONArray sysConfList = new JSONArray();
+        sysConfList.add(nowSysConfList);
+        sysConfList.add(upSysConfList);
         return sysConfList;
     }
 
